487656eedc9a621c3ec35d6446958b7f0e2fadd1
Jabarton <noblemonitor@gmail.com>
2022-05-12 19:07:57 +0500

    press E to start selecting a custom start and exit for a path

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import math
+from typing import Tuple, Optional
 
 import arcade
 
 import config as cfg
-from models import Maze
+from models import Maze, Node
 
 arcade.enable_timings()
 
@@ -21,24 +22,64 @@ class Game(arcade.Window):
         self.maze_iterations_per_frame = math.ceil(cfg.maze_width * cfg.maze_height / (60 * cfg.max_generation_time))
         self.maze_shape_list = arcade.ShapeElementList()
         self.path_shape_list = arcade.ShapeElementList()
+        self.is_selecting_start = False
+        self.is_selecting_exit = False
+        self.custom_start = None
 
     def on_key_press(self, symbol: int, modifiers: int):
+        if self.is_selecting_cell():
+            if symbol in (arcade.key.ESCAPE, arcade.key.E):
+                self.stop_selecting_cell()
+
+            return
+
         if symbol == arcade.key.ESCAPE:
             arcade.close_window()
         elif symbol == arcade.key.SPACE:
             self.generate_maze()
         elif symbol == arcade.key.ENTER:
             self.update_path()
+        elif symbol == arcade.key.E:
+            self.find_custom_path()
 
     def on_draw(self):
         self.clear()
 
         self.draw_maze()
         self.draw_path()
+        self.draw_start_marker()
+        self.draw_select_start_text()
+        self.draw_select_exit_text()
         self.show_fps()
 
     def on_update(self, delta_time: float):
         self.update_maze()
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        if self.is_selecting_cell():
+            lane_x, lane_y = self.select_cell(x, y)
+
+            if self.is_selecting_start:
+                self.custom_start = lane_x, lane_y
+                self.is_selecting_start = False
+                self.is_selecting_exit = True
+            else:
+                if (lane_x, lane_y) == self.custom_start:
+                    print('start and exit should be different')
+                    return
+
+                self.update_path(self.custom_start, (lane_x, lane_y))
+                self.stop_selecting_cell()
+
+    def reset(self):
+        self.is_generating = False
+        self.is_maze_generated = False
+        self.is_path_shown = False
+        self.maze_shape_list = arcade.ShapeElementList()
+        self.path_shape_list = arcade.ShapeElementList()
+        self.is_selecting_start = False
+        self.is_selecting_exit = False
+        self.custom_start = None
 
     def update_maze(self):
         if self.is_generating:
@@ -85,13 +126,10 @@ class Game(arcade.Window):
 
     def generate_maze(self):
         if not self.is_generating:
-            self.maze_shape_list = arcade.ShapeElementList()
-            self.path_shape_list = arcade.ShapeElementList()
+            self.reset()
             self.maze.reset_grid()
             self.is_generating = True
             self.maze_generator = self.maze.generate_with_dfs()
-            self.is_maze_generated = False
-            self.is_path_shown = False
 
     def draw_maze(self):
         self.maze_shape_list.draw()
@@ -103,16 +141,65 @@ class Game(arcade.Window):
     def show_fps():
         # Get FPS for the last 60 frames
         text = f"FPS: {arcade.get_fps(60):5.1f}"
-        arcade.draw_text(text, 10, 10, arcade.color.WHITE, 22)
+        arcade.draw_text(text, 10, 10, arcade.color.WHITE, 12)
 
-    def update_path(self):
+    def draw_select_start_text(self):
+        if self.is_selecting_start:
+            arcade.draw_text('Selecting start', self.width / 2, 10, arcade.color.WHITE, 16,
+                             anchor_x='center', anchor_y='baseline')
+
+    def draw_select_exit_text(self):
+        if self.is_selecting_exit:
+            arcade.draw_text('Selecting exit', self.width / 2, 10, arcade.color.WHITE, 16,
+                             anchor_x='center', anchor_y='baseline')
+
+    def draw_start_marker(self):
+        if self.is_selecting_exit and self.custom_start is not None:
+            x, y = self.custom_start
+            arcade.draw_circle_filled(*self.maze.g_cells[x][y],
+                                      cfg.cell_size * 0.85, cfg.path_color + (200,))
+
+    def update_path(self,
+                    start: Optional[Node] = None,
+                    end: Optional[Node] = None):
         if self.is_maze_generated and not self.is_path_shown:
-            path = self.maze.find_path()
+            path = self.maze.find_path(start, end)
             lines = arcade.create_line_strip([self.maze.g_cells[x][y] for x, y in path],
                                              cfg.path_color, line_width=cfg.path_width)
             self.path_shape_list.append(lines)
 
             self.is_path_shown = True
+
+    def find_custom_path(self):
+        if not self.is_maze_generated:
+            return
+
+        self.path_shape_list = arcade.ShapeElementList()
+        self.is_path_shown = False
+        self.is_selecting_start = True
+
+    def is_selecting_cell(self) -> bool:
+        return self.is_selecting_start or self.is_selecting_exit
+
+    def stop_selecting_cell(self):
+        self.is_selecting_start = False
+        self.is_selecting_exit = False
+        self.custom_start = None
+
+    def select_cell(self, mouse_x: float, mouse_y: float) -> Tuple[int, int]:
+        low_dist = float('inf')
+        low_x = 0
+        low_y = 0
+
+        for i, row in enumerate(self.maze.g_cells):
+            for j, (x, y) in enumerate(row):
+                dist = math.hypot(mouse_x - x, mouse_y - y)
+                if dist < low_dist:
+                    low_dist = dist
+                    low_x = i
+                    low_y = j
+
+        return low_x, low_y
 
 
 def main():
